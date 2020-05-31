5781aa2d8e71f8a6dd06e66e23bed9f37a91462d
Fernando <fernando22future@gmail.com>
2020-05-31 16:22:10 -0300

    Atualização da funcionalidade

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -1,1 +1,11 @@
 const quiz = require('../json/quiz1.json')
+
+function checar() {
+    let pergunta = document.getElementById("pergunta")
+    let alternativa1 = document.getElementById("alternativa1")
+    let alternativa2 = document.getElementById("alternativa2")
+    let alternativa3 = document.getElementById("alternativa3")
+    let alternativa4 = document.getElementById("alternativa4")
+
+    pergunta.innerHTML = "oi"
+}
